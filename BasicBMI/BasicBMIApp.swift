9eb39e88d455aa72593da773d9dc75d9e83152cb
Nipun-Luca <lmuhudugama@gmail.com>
2023-10-01 14:25:10 +0100

    Initial commit

---
+++ BasicBMI/BasicBMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicBMIApp.swift
+//  BasicBMI
+//
+//  Created by Nipun Luca muhudugama on 01/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BasicBMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
